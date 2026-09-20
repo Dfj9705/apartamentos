c17f6d62ccdc9466aa21fdad27864b44fa6eb2d3
Abner Daniel Fuentes Juárez <abnerfuentes05@gmail.com>
2026-09-19 20:28:05 -0600

    feat: personaliza logos de la app

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,9 @@
+@props(['url'])
+
+<tr>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            <img src="{{ asset('images/logo_apto.png') }}" class="logo" alt="AptoLogo">
+        </a>
+    </td>
+</tr>
